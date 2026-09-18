71a0098194f1b6406238bfbed2cb154691b8069c
Archu21-lab <archupatil5958@gmail.com>
2026-09-18 18:28:32 +0530

    ..

---
+++ employeeManagement/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/employeeManagement");
+    console.log("database connected successfully !");
+  } catch (err) {
+    console.log("database connction failed:ERROR -" + err.message);
+  }
+};
